245d329f546206636aa24b6c27b6a60957cab538
Johann Ortiz <johann9112@gmail.com>
2023-07-18 16:59:51 -0500

    Se establece las tareas a realizar

---
+++ Lista_Contactos.js
@@ -0,0 +1,15 @@
+/* Crea una lista de contactos con datos predefinidos, cada contacto debe
+ contener el nombre y apellido como una sola cadena de caracteres */
+
+
+
+
+//Crea una función para añadir un nuevo contacto a una lista
+
+
+
+//Crea una función para borrar un contacto existente de la lista
+
+
+
+//Crea una función para imprimir en consola los contactos presentes en la lista
